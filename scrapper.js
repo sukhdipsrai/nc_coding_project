8805bbd89adc3ef1c562847c6d501ed9516940f9
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-05-27 04:10:08 -0400

    created parsing function

--- scrapper.js
+++ scrapper.js
@@ -1,5 +1,5 @@
 const { fs } = require("fs");
-const database = require("./input.json");
+const inputJSON = require("./input.json");
 
 // let database = JSON.parse(fs.readFileSync("input.json", "utf-8"));
 let res = {
@@ -19,24 +19,59 @@ let res = {
     "100+": 0,
   },
 };
+function parseAndExtract(database) {
+  let states = {};
+  let ageGroup = [0, 0, 0, 0, 0, 0];
+  let total = 0;
+  for (let key in database.results) {
+    let value = database.results[key];
 
-let states = {};
+    let gender = value.gender;
+    let state = value.location.state;
+    let genderIndex = 1; // used for referencing the array which is keyed to state names in our states hash table
+    let firstName = value.name.first;
+    let lastName = value.name.last;
+    let age = value.dob.age;
+    total++; // keeping count of total names
+    if (key === "0") console.log(value); // TODO: DELETE in production
 
-for (let key in database.results) {
-  let value = database.results[key];
-  let gender = value.gender;
-  let state = value.location.state;
-  let genderIndex = 1;
-  // let total = database.results.size();
-  if (key === "0") console.log(value);
+    // female count
+    if (gender === "female") {
+      res.female++;
+      genderIndex = 0;
+      // "New York" : [ femaleCount++, maleCount++], see line 45
+    } else if (gender !== "male") genderIndex = 2; // documentation says only male and female genders, but that can change so adding small error saftey
 
-  if (gender === "female") {
-    res.female++;
-    genderIndex = 0;
+    // counting male and females for each state,
+    if (states[state] === undefined) states[state] = [0, 0, 0]; // inistialize key,value pair
+    states[state][genderIndex]++; // if genderIndex == 0 increment female count, == 1 increment male
+
+    // check first letter of firstname, a-m , n-z conditions
+    if (typeof firstName === "string" && charA_M(firstName[0]))
+      res.firstName["a-m"]++;
+
+    // check first letter of lastname, a-m , n-z conditions
+    if (typeof lastName === "string" && charA_M(lastName[0]))
+      res.lastName["a-m"]++;
+
+    // Sort Ages into the ageGroup Array
+    if (age > 100) ageGroup[5]++;
+    else ageGroup[Math.ceil(age / 20) - 1]++;
   }
 
-  if (states[state] === undefined) states[state] = [0, 0];
-  states[state][genderIndex]++;
+  // Assign the array elements into our response JSON
+  res.age["0-20"] = ageGroup[0];
+  res.age["21-40"] = ageGroup[1];
+  res.age["41-60"] = ageGroup[2];
+  res.age["61-80"] = ageGroup[3];
+  res.age["81-100"] = ageGroup[4];
+  res.age["100+"] = ageGroup[5];
 }
 
-console.log(states);
+function charA_M(char) {
+  // if (typeof char !== "string") return false; use before input
+  let code = char[0].toLowerCase().charCodeAt(0);
+  return code >= 97 && code <= 109;
+}
+
+console.log(res);
