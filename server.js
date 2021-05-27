e0908dfc4878ed7d38895ec73cd2890effd103c3
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-05-27 17:38:51 -0400

    formated backend api design for different file output

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
-app.use(express.urlencoded({ limit: "50mb", parameterLimit: 500000000 }));
+// app.use(express.urlencoded({ limit: "50mb", parameterLimit: 500000000 }));
 app.use(express.json({ limit: "50mb", parameterLimit: 500000000 }));
 
 app.get("/express_backend", (req, res) => {
@@ -18,7 +18,12 @@ app.get("/express_backend", (req, res) => {
 // text/xml && application/xml
 app.get("/api/randomUser", (req, res) => {
   let database = req.body;
-  console.log(randomPeople.parseAndExtract(database));
-
+  // console.log(randomPeople.parseAndExtract(database));
+  console.log(req.headers.accept === "text/plain");
+  console.log(
+    req.headers.accept === "text/xml" ||
+      req.headers.accept === "application/xml"
+  );
+  // TODO: if text, do txt format,  if xml do xml format, else return as is (JSON)
   res.send("test");
 });
