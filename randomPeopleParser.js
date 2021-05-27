7a8d7cd967bc42508fe7700598ef36d48dbdfbd1
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-05-27 15:56:17 -0400

    exported parser as module and created exposed api

--- randomPeopleParser.js
+++ randomPeopleParser.js
@@ -1,106 +1,109 @@
-const { fs } = require("fs");
-let inputJSON = require("./input.json");
-
+// const { fs } = require("fs");
+// let inputJSON = require("./input.json");
 // let database = JSON.parse(fs.readFileSync("input.json", "utf-8"));
 
-function parseAndExtract(database) {
-  let res = {
-    female: 0,
-    firstName: {
-      "a-m": 0,
-    },
-    lastName: {
-      "a-m": 0,
-    },
-    age: {
-      "0-20": 0,
-      "21-40": 0,
-      "41-60": 0,
-      "61-80": 0,
-      "81-100": 0,
-      "100+": 0,
-    },
-  };
-  let states = {};
-  let ageGroup = [0, 0, 0, 0, 0, 0];
-  let total = 0;
-  for (let key in database.results) {
-    let value = database.results[key];
-
-    let gender = value.gender;
-    let state = value.location.state;
-    let genderIndex = 1; // used for referencing the array which is keyed to state names in our states hash table
-    let firstName = value.name.first;
-    let lastName = value.name.last;
-    let age = value.dob.age;
-    total++; // keeping count of total names
-    // if (key === "0") console.log(value); // TODO: DELETE in production
-
-    // female count
-    if (gender === "female") {
-      res.female++;
-      genderIndex = 0;
-      // "New York" : [ femaleCount++, maleCount++], see line 45
-    } else if (gender !== "male") genderIndex = 2; // documentation says only male and female genders, but that can change so adding small error saftey
-
-    // counting male and females for each state,
-    if (states[state] === undefined) states[state] = [0, 0, 0]; // inistialize key,value pair
-    states[state][genderIndex]++; // if genderIndex == 0 increment female count, == 1 increment male
-
-    // check first letter of firstname, a-m , n-z conditions
-    if (typeof firstName === "string" && charA_M(firstName[0]))
-      res.firstName["a-m"]++;
-
-    // check first letter of lastname, a-m , n-z conditions
-    if (typeof lastName === "string" && charA_M(lastName[0]))
-      res.lastName["a-m"]++;
-
-    // Sort Ages into the ageGroup Array
-    if (age > 100) ageGroup[5]++;
-    else ageGroup[Math.ceil(age / 20) - 1]++;
-  } // end loop through database JSON
-
-  // convert current stats into percent
-  res.female = percentAndRound(res.female / total);
-  res.firstName["a-m"] = percentAndRound(res.firstName["a-m"] / total);
-  res.lastName["a-m"] = percentAndRound(res.lastName["a-m"] / total);
-
-  // Create and Array of states and their total population
-  statesArr = [];
-  Object.keys(states).forEach((k) => {
-    let v = states[k];
-    statesArr.push([k, v[0] + v[1] + v[2]]);
-  });
-
-  // Sort the States Array
-  statesArr = statesArr.sort((first, second) => {
-    if (first[1] === second[1]) return first[0] < second[0] ? -1 : 1;
-    else return first[1] > second[1] ? -1 : 1;
-  });
-
-  // Grab upto the first 10 and JSON-fy
-  let topState = {};
-  for (let i = 0; i < 10 && i < statesArr.length; i++) {
-    let k = statesArr[i][0];
-    let stateTotal = statesArr[i][1];
-    topState[k] = {
-      female: percentAndRound(states[k][0] / stateTotal),
-      male: percentAndRound(states[k][1] / stateTotal),
-      total: percentAndRound(statesArr[i][1] / total),
+module.exports = {
+  parseAndExtract: function (database) {
+    let res = {
+      female: 0,
+      firstName: {
+        "a-m": 0,
+      },
+      lastName: {
+        "a-m": 0,
+      },
+      age: {
+        "0-20": 0,
+        "21-40": 0,
+        "41-60": 0,
+        "61-80": 0,
+        "81-100": 0,
+        "100+": 0,
+      },
     };
-  }
-  res.states = topState;
+    let states = {};
+    let ageGroup = [0, 0, 0, 0, 0, 0];
+    let total = 0;
+    for (let key in database.results) {
+      let value = database.results[key];
 
-  // Assign the array elements into our response JSON
-  res.age["0-20"] = ageGroup[0];
-  res.age["21-40"] = ageGroup[1];
-  res.age["41-60"] = ageGroup[2];
-  res.age["61-80"] = ageGroup[3];
-  res.age["81-100"] = ageGroup[4];
-  res.age["100+"] = ageGroup[5];
-  return res;
-}
+      let gender = value.gender;
+      let state = value.location.state;
+      let genderIndex = 1; // used for referencing the array which is keyed to state names in our states hash table
+      let firstName = value.name.first;
+      let lastName = value.name.last;
+      let age = value.dob.age;
+      total++; // keeping count of total names
+      // if (key === "0") console.log(value); // TODO: DELETE in production
 
+      // female count
+      if (gender === "female") {
+        res.female++;
+        genderIndex = 0;
+        // "New York" : [ femaleCount++, maleCount++], see line 45
+      } else if (gender !== "male") genderIndex = 2; // documentation says only male and female genders, but that can change so adding small error saftey
+
+      // counting male and females for each state,
+      if (states[state] === undefined) states[state] = [0, 0, 0]; // inistialize key,value pair
+      states[state][genderIndex]++; // if genderIndex == 0 increment female count, == 1 increment male
+
+      // check first letter of firstname, a-m , n-z conditions
+      if (typeof firstName === "string" && charA_M(firstName[0]))
+        res.firstName["a-m"]++;
+
+      // check first letter of lastname, a-m , n-z conditions
+      if (typeof lastName === "string" && charA_M(lastName[0]))
+        res.lastName["a-m"]++;
+
+      // Sort Ages into the ageGroup Array
+      if (age > 100) ageGroup[5]++;
+      else ageGroup[Math.ceil(age / 20) - 1]++;
+    } // end loop through database JSON
+
+    // convert current stats into percent
+    res.female = percentAndRound(res.female / total);
+    res.firstName["a-m"] = percentAndRound(res.firstName["a-m"] / total);
+    res.lastName["a-m"] = percentAndRound(res.lastName["a-m"] / total);
+
+    // Create and Array of states and their total population
+    statesArr = [];
+    Object.keys(states).forEach((k) => {
+      let v = states[k];
+      statesArr.push([k, v[0] + v[1] + v[2]]);
+    });
+
+    // Sort the States Array
+    statesArr = statesArr.sort((first, second) => {
+      if (first[1] === second[1]) return first[0] < second[0] ? -1 : 1;
+      else return first[1] > second[1] ? -1 : 1;
+    });
+
+    // Grab upto the first 10 and JSON-fy
+    let topState = {};
+    for (let i = 0; i < 10 && i < statesArr.length; i++) {
+      let k = statesArr[i][0];
+      let stateTotal = statesArr[i][1];
+      topState[k] = {
+        female: percentAndRound(states[k][0] / stateTotal),
+        male: percentAndRound(states[k][1] / stateTotal),
+        total: percentAndRound(statesArr[i][1] / total),
+      };
+    }
+    res.states = topState;
+
+    // Assign the array elements into our response JSON
+    res.age["0-20"] = ageGroup[0];
+    res.age["21-40"] = ageGroup[1];
+    res.age["41-60"] = ageGroup[2];
+    res.age["61-80"] = ageGroup[3];
+    res.age["81-100"] = ageGroup[4];
+    res.age["100+"] = ageGroup[5];
+    return res;
+  },
+};
+// console.log(parseAndExtract(inputJSON));
+
+// true only if character is A-M
 function charA_M(char) {
   // if (typeof char !== "string") return false; use before input
   let code = char[0].toLowerCase().charCodeAt(0);
@@ -111,5 +114,3 @@ function charA_M(char) {
 function percentAndRound(value) {
   return Number((value * 100).toFixed(2));
 }
-
-// console.log(parseAndExtract(inputJSON));
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const randomPeople = require("./randomPeopleParser");
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.use(express.urlencoded({ limit: "50mb", parameterLimit: 500000000 }));
+app.use(express.json({ limit: "50mb", parameterLimit: 500000000 }));
 
 app.get("/express_backend", (req, res) => {
   console.log(req.headers.accept);
@@ -12,6 +16,9 @@ app.get("/express_backend", (req, res) => {
 //  text/plain
 //  application/json
 // text/xml && application/xml
-app.post("/api/randomUser", (req, res) => {
+app.get("/api/randomUser", (req, res) => {
+  let database = req.body;
+  console.log(randomPeople.parseAndExtract(database));
+
   res.send("test");
 });
