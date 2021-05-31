b5cafb067cb06e5fe3568cf6f7ebc7c36d152796
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-05-31 06:29:36 -0400

    added file and input selector/swithcer on homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,18 +3,47 @@ import "../stylesheets/homepage.css";
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
+    this.switchInput = this.switchInput;
+    this.state = { text: false };
   }
+
+  switchInput = function () {
+    this.setState({ text: !this.state.text });
+  };
 
   render() {
     return (
       <div className="homepage-container">
         <h2>Random User Data Visualiser</h2>
-        <input type="radio" id="text" name="input-type" value="text"></input>
+        <input
+          type="radio"
+          id="text"
+          name="input-type"
+          value="text"
+          defaultChecked="checked"
+          onChange={(e) => this.switchInput()}
+        ></input>
         <label htmlFor="text">Text</label>
-        <input type="radio" id="file" name="input-type" value="file"></input>
+        <input
+          type="radio"
+          id="file"
+          name="input-type"
+          value="file"
+          selected={this.state.text}
+          onChange={(e) => this.switchInput()}
+        ></input>
         <label htmlFor="file">File</label>
-        <textarea className="json-text-input"></textarea>
-        <input type="file" accept=".json" className="json-file-input"></input>
+
+        <textarea
+          className="json-text-input"
+          disabled={this.state.text}
+        ></textarea>
+        <input
+          type="file"
+          accept=".json"
+          className="json-file-input"
+          disabled={!this.state.text}
+        ></input>
         <input
           type="submit"
           className="json-visualize"
