428850a68c5fe4c465342cda0c45c38e3cb762a8
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-06-01 17:36:11 -0400

    typo in build path

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("client/build"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
