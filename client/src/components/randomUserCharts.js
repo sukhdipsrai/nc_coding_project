bfea327e95d5e8fe8180f00fad3d22144e60bfa3
Sukhdip Rai <sukhdipsrai@gmail.com>
2021-06-01 03:32:07 -0400

    gender chart done

--- client/src/components/randomUserCharts.js
+++ client/src/components/randomUserCharts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../stylesheets/homepage.css";
+import { Pie } from "react-chartjs-2";
 
 class RandomUserCharts extends React.Component {
   constructor(props) {
@@ -7,7 +8,25 @@ class RandomUserCharts extends React.Component {
   }
 
   render() {
-    return <div className="charts-container">{console.log(this.props)}</div>;
+    console.log(this.props.stats.results);
+    let { female, firstName, lastName, age, states } = this.props.stats.results;
+    let genderData = {
+      datasets: [
+        {
+          label: "% of total population",
+          data: [female, 100.0 - female],
+          backgroundColor: ["rgba(93, 78, 109,.5)", "rgba(201, 242, 153,0.5"],
+        },
+      ],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: ["Female", "Male"],
+    };
+    return (
+      <div className="charts-container">
+        <Pie id="gender" data={genderData}></Pie>
+      </div>
+    );
   }
 }
 
